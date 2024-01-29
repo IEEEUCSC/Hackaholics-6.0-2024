3594c297f1286dba3f1f0daf9a595c84fb07ac03
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-01-29 13:07:45 +0530

    FAQ Added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Footer from './Components/Footer/Footer';
 import Prizes from './Components/Prizes/Prizes';
 import './App.css';
 import Navbar from './Components/NavBar/NavBar';
+import FAQ from './Components/FAQ/FAQ';
 
 
 function App() {
