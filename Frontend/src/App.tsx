a2b2f03a5470a987923a629810783b0ea85b8639
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-01-29 10:10:37 +0530

    Revert "Currently Doing FAQ Section"
    This reverts commit d61cc93faff198a1384ca851fcafbd33a136279c.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Footer from './Components/Footer/Footer';
 import Prizes from './Components/Prizes/Prizes';
-import FAQ from './Components/FAQ/FAQ';
 import './App.css';
 
 function App() {
   return (
    <div className='content'>
-    <FAQ />
     <Prizes />
     <Footer />
    </div>
