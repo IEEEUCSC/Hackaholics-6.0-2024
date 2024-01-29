ef7e3930195dfcfad7ff80602fabb329e49ab5c1
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-01-29 10:23:13 +0530

    Navbar added to main

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './Components/Footer/Footer';
 import Prizes from './Components/Prizes/Prizes';
+import Navbar from './Components/NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
    <div className='content'>
+    <Navbar />
     <Prizes />
     <Footer />
    </div>
