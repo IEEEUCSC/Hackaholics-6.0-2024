8a956f43364687ad0677e72b92292de4459d48c4
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-02-01 00:12:42 +0530

    Refactor NavBar component to conditionally render "Help Me" item and "Register Now" button

--- Frontend/src/Components/NavBar/NavBar.tsx
+++ Frontend/src/Components/NavBar/NavBar.tsx
@@ -14,14 +14,24 @@ const Navbar: React.FC = () => {
 
     return (
         <div
-            className={`fixed top-0 w-screen bg-secondary bg-opacity-90 z-10 ${isOpen ? "h-screen overflow-hidden" : ""
+            className={`fixed top-0 w-screen bg-secondary bg-opacity-90 z-10 ${isOpen ? "" : "overflow-hidden"
                 }`}
         >
             <div className="flex flex-col md:flex-row justify-between items-right ml-0 md:ml-1 mr-1 md:mr-0 mb-0 px-2 md:px-9 py-3 md:py-5">
                 <div className="flex items-center">
-                    <a href="https://github.com/IEEEUCSC" className="block md:hidden">
+                    <div className="md:hidden">
+                        <IconButton
+                            onClick={toggleMenu}
+                            color="inherit"
+                            sx={{ color: isOpen ? "gray" : "white" }}
+                        >
+                            {isOpen ? <CloseIcon /> : <MenuIcon />}
+                        </IconButton>
+                    </div>
+
+                    <a href="https://github.com/IEEEUCSC" className="block md:hidden ml-2">
                         <img
-                            className="relative inline-block h-20 ml-auto -mt-2"
+                            className="relative inline-block h-20 -mt-2"
                             src={mobileLogo}
                             alt="mobile-logo"
                         />
@@ -35,15 +45,7 @@ const Navbar: React.FC = () => {
                         />
                     </a>
 
-                    <div className="md:hidden flex justify-end items-center">
-                        <IconButton
-                            onClick={toggleMenu}
-                            color="inherit"
-                            sx={{ color: isOpen ? "gray" : "white" }}
-                        >
-                            {isOpen ? <CloseIcon /> : <MenuIcon />}
-                        </IconButton>
-                    </div>
+                    <div className="md:hidden flex justify-end items-center"></div>
                 </div>
                 <div
                     className={`md:flex flex-col md:flex-row md:space-y-0 space-y-2 text-xl bg-opacity-10 whitespace-nowrap ${isOpen ? "block border-2 border-gray-100 rounded-lg" : "hidden"
@@ -54,22 +56,29 @@ const Navbar: React.FC = () => {
                         { label: "Time Line", link: "https://github.com/IEEEUCSC" },
                         { label: "Prize", link: "https://github.com/IEEEUCSC" },
                         { label: "Contact Us", link: "https://github.com/IEEEUCSC" },
+                        { label: "Help Me", link: "https://github.com/IEEEUCSC" },
                     ].map((item, index) => (
-                        <a key={index} href={item.link} className="group relative md:mr-9">
-                            <div className="py-3 md:py-2 px-3 indigo-950 text-white hover:text-purple-300 active:bg-indigo-600 active:text-white text-center">
-                                {item.label}
-                                <div className="absolute inset-x-0 bottom-0 h-0.5 bg-white transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 active:bg-indigo-600 active:text-white"></div>
-                            </div>
-                        </a>
-                    ))}
+                        // Conditionally render "Help Me" item only for mobile view
+                        !(item.label === "Help Me" && !isOpen) && (
+                            <a key={index} href={item.link} className="group relative md:mr-9">
+                                <div className="py-3 md:py-2 px-3 indigo-950 text-white hover:text-purple-300 active:bg-indigo-600 active:text-white text-center">
+                                    {item.label}
+                                    <div className="absolute inset-x-0 bottom-0 h-0.5 bg-white transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 active:bg-indigo-600 active:text-white"></div>
+                                </div>
+                            </a>
+                        )
+                    )}
 
-                    <a
-                        href="#_"
-                        className="relative rounded px-6 py-2 overflow-hidden group bg-secondary hover:bg-gradient-to-r hover:from-violet-700 hover:to-purple-900 text-white hover:ring-1 hover:ring-offset-1 hover:ring-white transition-all ease-out duration-500 md:align-middle md:items-center mx-auto"
-                    >
-                        <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-tertiary opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-                        <span className="relative text-center">Register Now</span>
-                    </a>
+                    {/* Conditionally render Register Now button for non-mobile screens */}
+                    {!isOpen && (
+                        <a
+                            href="#_"
+                            className="relative rounded px-6 py-2 overflow-hidden group bg-secondary hover:bg-gradient-to-r hover:from-violet-700 hover:to-purple-900 text-white hover:ring-1 hover:ring-offset-1 hover:ring-white transition-all ease-out duration-500 md:align-middle md:items-center mx-auto"
+                        >
+                            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-tertiary opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                            <span className="relative text-center">Register Now</span>
+                        </a>
+                    )}
                 </div>
             </div>
         </div>
