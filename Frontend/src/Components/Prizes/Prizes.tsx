46c0d5cd5e87fd59cbcfbce2afaa6b05029ccccb
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-02-02 18:45:15 +0530

    Topics Added for every page

--- Frontend/src/Components/Prizes/Prizes.tsx
+++ Frontend/src/Components/Prizes/Prizes.tsx
@@ -4,7 +4,18 @@ import "./Prizes.css";
 const Prizes = () => {
   return (
     <div className="prizeDiv">
-      {/* <h1 className="prizeHeading">Awards for Winners</h1> */}
+      <div className="prizeHead">
+        <div
+          className="shape"
+          data-aos="zoom-in-left"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <h1>
+            <span>Awards for Winners</span>
+          </h1>
+        </div>
+      </div>
       <div className="prizeRow">
         <div className="prizeColOne">
           <div className="prizeDetailsContainor">
@@ -14,8 +25,12 @@ const Prizes = () => {
         </div>
         <div className="prizeColTwo">
           <div className="prizeDetailsContainor">
-            <h2 className="prizeTitle" id="prizeTitle">1st Place</h2>
-            <h3 className="prizeText" id="prizeText">Rs. 50,000</h3>
+            <h2 className="prizeTitle" id="prizeTitle">
+              1st Place
+            </h2>
+            <h3 className="prizeText" id="prizeText">
+              Rs. 50,000
+            </h3>
           </div>
         </div>
         <div className="prizeColThree">
