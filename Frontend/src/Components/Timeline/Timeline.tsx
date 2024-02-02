5d739289b451b12cf27b11f1f17a5e278095f184
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2024-02-02 23:27:56 +0530

    Mobile Responsive Done

--- Frontend/src/Components/Timeline/Timeline.tsx
+++ Frontend/src/Components/Timeline/Timeline.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  VerticalTimeline,
+  // VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
