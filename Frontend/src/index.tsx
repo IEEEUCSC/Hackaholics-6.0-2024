b692a498e6f33c6297308af0f22097ed184201b3
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2024-02-03 00:18:05 +0530

    Add

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>      
-    <App />
-    </HashRouter>
+
+
+      <App />
+
+    
   </React.StrictMode>
 );
 
