b44629ff529357caa73b6049b9335130f98c1888
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-02-04 12:24:36 +0530

    Update Footer component styling and add social links

--- Frontend/src/Components/Footer/Footer.tsx
+++ Frontend/src/Components/Footer/Footer.tsx
@@ -1,51 +1,73 @@
 import ucscwie from '../../Assets/Footer/wie-ucsc.png';
 import ieeewiee from '../../Assets/Footer/ieee-wie.png';
-import "./Footer.css";
+import ucscwie2 from '../../Assets/Footer/wie-ucsc2.png';
+import ieeewiee2 from '../../Assets/Footer/ieee-wie2.png';
+import logo from '../../Assets/NavBar/logo.png'
+
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary text-white bg-opacity-100">
-      <div className="container mx-auto sm-auto px-6 py-12 space-x-1" id={"footer"}>
-        <div className="flex flex-col mt-1">
-{/* First Row */}
-          <div className="flex justify-center">
-            <div className="flex items-center justify-center">
-              <img className="inline-block h-60 w-50 mt-1 ml-0.5 mr-0.5 rounded-full" src={ieeewiee} alt="wie-logo" />
-            </div>
-            <div className="flex items-center justify-center" id='footerLogoDiv'>
-              <img className="inline-block h-60 w-50 mt-1 ml-0.5 mr-0.5 rounded-full" src={ucscwie} alt="wie-logo" />
-            </div>
+    <main className='flex flex-col py-12 items-center content-center justify-center'>
+      <div>
+        {/* Logos */}
+        <div className='flex flex-row justify-center gap-32 mb-16'>
+          {/* First Image */}
+          <div>
+            <img className="inline-block w-32 mt-1 ml-0.5 mr-0.5" src={ieeewiee2} alt="wie-logo" />
+          </div>
+          {/* Second Image - Hackaholics Logo */}
+
+          {/* <div>
+            <img className="inline-block w-80 mt-10 ml-0.5 mr-0.5" src={logo} alt="wie-logo" />
+          </div> */}
+
+          {/* Third Image */}
+          <div>
+            <img className="inline-block w-32 mt-1 ml-0.5 mr-0.5" src={ucscwie2} alt="wie-logo" />
           </div>
 
-{/* Second Row - Text Links */}
-          <div className="flex flex-row justify-center my-4 space-x-12" id='footerLinks'>
-            <div>
-              <a href="https://github.com/IEEEUCSC">
-                <div className="text-xl hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize " id='footerLink'>Official WebSite</div>
-              </a>
-            </div>
-            <div>
-              <a href="https://github.com/IEEEUCSC">
-                <div className="text-xl hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize "id='footerLink'>LinkedIn</div>
-              </a>
-            </div>
-            <div>
-              <a href="https://github.com/IEEEUCSC">
-                <div className="text-xl hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize "id='footerLink'>Instagram</div>
-              </a>
-            </div>
-            <div>
-              <a href="https://github.com/IEEEUCSC">
-                <div className="text-xl hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize "id='footerLink'>FaceBook</div>
-              </a>
-            </div>
-          </div>
         </div>
-        <p className="text-xm text-cyan-950 mt-5 hover:text-tertiary text-center" id='footerRights'>
-          © {new Date().getFullYear()} IEEE- UCSC All rights reserved.
-        </p>
+
+        {/* Socila Links */}
+        <div className='flex flex-row justify-center gap-14 text-white font-pfont tracking-widest text-2xl'>
+          <div className=''>
+            <a href="https://github.com/IEEEUCSC">
+              <div className="text-1xl p-1 hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize " id='footerLink'>WebSite</div>
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/IEEEUCSC">
+              <div className="text-2xl p-1 hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize " id='footerLink'>LinkedIn</div>
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/IEEEUCSC">
+              <div className="text-2xl p-1 hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize " id='footerLink'>Instagram</div>
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/IEEEUCSC">
+              <div className="text-2xl p-1 hover:text-violet-400 text-white text-center transition-transform transform hover:scale-90 capitalize " id='footerLink'>FaceBook</div>
+            </a>
+          </div>
+
+
+
+        </div>
+
+        {/* Copy right Part */}
+        <div>
+          <p className="text-xm font-pfont text-tertiary mt-14 hover:text-white text-center">
+            © {new Date().getFullYear()} IEEE - UCSC All Rights Reserved.
+          </p>
+
+        </div>
+
+
+
       </div>
-    </footer>
+    </main>
+
   );
 };
 
--- Frontend/src/Components/NavBar/NavBar.tsx
+++ Frontend/src/Components/NavBar/NavBar.tsx
@@ -28,7 +28,10 @@ const Navbar: React.FC = () => {
                         />
                     </a>
 
-                    <a href="https://github.com/IEEEUCSC" className="hidden md:block mt-1">
+                    <a
+                        href="https://github.com/IEEEUCSC"
+                        className="hidden md:block mt-1"
+                    >
                         <img
                             className="inline-block h-10 ml-2 mt-0 md:mr-10"
                             src={logo}
@@ -47,37 +50,44 @@ const Navbar: React.FC = () => {
                     </div>
                 </div>
                 <div className="">
-                <div
-                    className={`md:flex flex-col md:flex-row md:space-y-0 space-y-1 text-xl  flex justify-end items-center absolute top- right-10 whitespace-nowrap ${isOpen ? "block box-border border-primary h-60 w-44  md:absolute md:items-center md:top-7 md:right-10 border-2 font-pfont rounded-lg" : "hidden"
-                        }`}
-                >
-                    {[
-                        { label: "Home", link: "/#home" },
-                        { label: "Time Line", link: "/#time" },
-                        { label: "Prizes", link: "/#price" },
-                        { label: "Register Now", link: "https://github.com/IEEEUCSC" },
-                    ].map((item, index) => (
-
-                        !(item.label === "Register Now" && !isOpen) && (
-                            <a key={index} href={item.link} className="group relative md:mr-9">
-                                <div className="py-3 md:py-2 px-3 md:mt-2 z-30 indigo-950 text-white hover:text-purple-300 active:bg-indigo-600 active:text-white text-center">
-                                    {item.label}
-                                    <div className="absolute inset-x-0 bottom-0 h-0.5 bg-white transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 active:bg-indigo-600 active:text-white"></div>
-                                </div>
+                    <div
+                        className={`md:flex flex-col md:flex-row md:space-y-0 space-y-1 text-xl  flex justify-end items-center absolute top- right-10 whitespace-nowrap ${isOpen
+                                ? "block box-border border-primary h-60 w-44  md:absolute md:items-center md:top-7 md:right-10 border-2 font-pfont rounded-lg"
+                                : "hidden"
+                            }`}
+                    >
+                        {[
+                            { label: "Home", link: "/#home" },
+                            { label: "Time Line", link: "/#time" },
+                            { label: "Prizes", link: "/#price" },
+                            { label: "Register Now", link: "https://github.com/IEEEUCSC" },
+                        ].map(
+                            (item, index) =>
+                                !(item.label === "Register Now" && !isOpen) && (
+                                    <a
+                                        key={index}
+                                        href={item.link}
+                                        className="group relative md:mr-9"
+                                    >
+                                        <div className="py-3 md:py-2 px-3 md:mt-2 z-30 indigo-950 text-white hover:text-purple-300 active:bg-indigo-600 active:text-white text-center">
+                                            {item.label}
+                                            <div className="absolute inset-x-0 bottom-0 h-0.5 bg-white transition-all duration-300 transform scale-x-0 group-hover:scale-x-100 active:bg-indigo-600 active:text-white"></div>
+                                        </div>
+                                    </a>
+                                )
+                        )}
+                        {!isOpen && (
+                            <a
+                                href="/team"
+                                className="relative rounded pt-3  px-3 py-1 overflow-hidden group bg-secondary hover:bg-gradient-to-r hover:from-violet-700 hover:to-purple-900 text-white hover:ring-1 hover:ring-offset-1 hover:ring-white transition-all ease-out duration-500 md:align-middle md:items-center mx-auto" // Add mt-2 to move the button up
+                            >
+                                <span className="absolute right-0 -pb-2 w-8 h-32 transition-all duration-1000 transform translate-x-12 bg-tertiary opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                                <span className="relative text-center p-2 pb-3 tracking-widest">
+                                    Register Now
+                                </span>
                             </a>
-                        )
-                    ))}
-                    {!isOpen && (
-                        <a
-                            href="/team"
-                            className="relative rounded pt-3  px-3 py-1 overflow-hidden group bg-secondary hover:bg-gradient-to-r hover:from-violet-700 hover:to-purple-900 text-white hover:ring-1 hover:ring-offset-1 hover:ring-white transition-all ease-out duration-500 md:align-middle md:items-center mx-auto"  // Add mt-2 to move the button up
-                        >
-                            <span className="absolute right-0 -pb-2 w-8 h-32 transition-all duration-1000 transform translate-x-12 bg-tertiary opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-                            <span className="relative text-center p-2 pb-3 tracking-widest">Register Now</span>
-                        </a>
-
-                    )}
-            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
